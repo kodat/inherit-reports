1d388a14c3c5b1d58cb7e0c9433ce4b9ba6becd6
Bashir Alsuty <beshasuty125@hotmail.com>
2016-12-11 06:48:17 -0800

    Update __openerp__.py

--- inherit_reports/__openerp__.py
+++ inherit_reports/__openerp__.py
@@ -1,1 +1,9 @@
-
+# -*- coding: utf-8 -*-
+{
+	'name': 'Inherit Reports',
+	'author': "Bashir Idirs(Alsuty)",
+	'installable':True,
+	'depends':['account'],
+	'data': ['inherit_account_invoice_report.xml'],
+	
+}
